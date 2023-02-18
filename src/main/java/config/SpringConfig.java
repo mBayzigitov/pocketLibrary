979ea270c1d0df50fcf4469d4eeeed31ca9b79ab
mBayzigitov <bayzigitov.murat@gmail.com>
2023-02-18 23:33:35 +0300

    Part of views added

--- src/main/java/config/SpringConfig.java
+++ src/main/java/config/SpringConfig.java
@@ -11,6 +11,7 @@ import org.springframework.core.env.Environment;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.thymeleaf.spring5.SpringTemplateEngine;
@@ -73,6 +74,14 @@ public class SpringConfig implements WebMvcConfigurer {
         dataSource.setPassword(environment.getProperty("PASSWORD"));
 
         return dataSource;
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/css/**")
+                .addResourceLocations("/WEB-INF/css/");
+        registry.addResourceHandler("/img/**")
+                .addResourceLocations("/WEB-INF/img/");
     }
 
     @Bean
